2ffc325109e588bd075672738fea4a6a39231280
evan-carlsen <evan@crowdbotics.com>
2020-11-15 13:07:50 -0800

    Update screen

--- src/features/BlankScreen014549/screens/blank.js
+++ src/features/BlankScreen014549/screens/blank.js
@@ -27,7 +27,7 @@ export default class Blank extends React.Component {
 
   render = () => (
     <View style={styles.container}>
-      <Text>This is your new component</Text>
+      <Text>This is the default blank component. IGNORE..</Text>
     </View>
   );
 }
