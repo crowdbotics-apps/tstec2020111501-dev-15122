459927b0a8828b1a699acd9300f8a0df30ffe1b0
evan-carlsen <evan@crowdbotics.com>
2020-11-15 14:20:12 -0800

    Update screen

---
+++ src/component/Button/styles.js
@@ -0,0 +1,19 @@
+import Color from '../../styles/colors;
+export default {
+  viewStyle: {
+    backgroundColor: Color.malibu,
+    borderRadius: 10,
+    borderColor: Color.black,
+    justifyContent: 'center',
+    margin: 10,
+  },
+  textStyle: {
+    fontSize: 16,
+    textAlign: 'center',
+    color: Color.white,
+    marginHorizontal: 40,
+    marginVertical: 12,
+    fontFamily: 'OpenSans-Regular',
+    //fontWeight: '800',
+  },
+};
