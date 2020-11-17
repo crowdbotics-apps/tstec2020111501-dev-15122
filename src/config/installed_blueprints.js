b44700760251a93a37faea075f209e3b4335faab
evan-carlsen <60202545+evan-carlsen@users.noreply.github.com>
2020-11-17 13:23:08 -0800

    Update installed_blueprints.js

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,6 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
-{ name: 'BlankScreen014549', human_name: 'Blank Screen 0', access_route: 'BlankScreen014549'},
+{ name: 'EmailAuth', human_name: 'Sign In / Sign Up', access_route: 'EmailAuth'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
