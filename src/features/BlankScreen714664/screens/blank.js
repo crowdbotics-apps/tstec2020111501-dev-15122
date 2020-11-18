8c94e8303c02e077a33945fc5cac35f8029d1fb7
evan-carlsen <evan@crowdbotics.com>
2020-11-18 14:43:20 -0800

    Update screen

--- src/features/BlankScreen714664/screens/blank.js
+++ src/features/BlankScreen714664/screens/blank.js
@@ -27,7 +27,7 @@ export default class Blank extends React.Component {
 
   render = () => (
     <View style={styles.container}>
-      <Text>This is your new component</Text>
+      <Text>PRIVACY POLICY SCREEN</Text>
     </View>
   );
 }
